5bf74ab38a4b42b028cf83c1b4b1c29a01372714
Krystian Stopka <kr6stopka@gmail.com>
2022-04-12 12:43:27 +0200

    start portfolio

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { FunctionComponent } from "react";
+
+interface AppProps {}
+
+const App: FunctionComponent<AppProps> = () => {
+    return (
+        <div className="wrapper">
+            <h1>ksotpka</h1>
+        </div>
+    );
+};
+
+export default App;
