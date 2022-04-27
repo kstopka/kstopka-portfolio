c58a7663c10c0135af651333cee9b7caa715e584
Krystian Stopka <kr6stopka@gmail.com>
2022-04-27 15:44:39 +0200

    add navigation, hamburger, logo

--- src/components/layout/Navigation/index.tsx
+++ src/components/layout/Navigation/index.tsx
@@ -1,23 +1,19 @@
 import * as React from "react";
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { Link } from "react-router-dom";
 import Footprint from "../../standard/Home/images/footprint-304372_640.svg";
 import { NavigationProps } from "./types.d";
 
 import styles from "./rwd.module.scss";
 
-const { nav } = styles;
+const { nav, navLogo, navHamburger, navHamburgerIcon, navHamburgerIconOpen } = styles;
 
 const Navigation: FunctionComponent<NavigationProps> = () => {
+    const [opened, setOpened] = useState(false);
     return (
         <div>
             {/* <div className="nav">
                 <div className="nav__container">
-                    <div className="nav__container__logo">
-                        <Link to="/">
-                            <img src={Footprint} alt="Footprint" />
-                        </Link>
-                    </div>
                     <ul className="nav__container__links">
                         <li>
                             <div className="nav__container__link">
@@ -36,7 +32,16 @@ const Navigation: FunctionComponent<NavigationProps> = () => {
                     </ul>
                 </div>
             </div> */}
-            <div className={nav}></div>
+            <div className={nav}>
+                <div className={navLogo}>
+                    <Link to="/">
+                        <img src={Footprint} alt="Footprint" />
+                    </Link>
+                </div>
+                <div className={navHamburger} onClick={() => setOpened((prevState) => !prevState)}>
+                    <div className={opened ? navHamburgerIconOpen : navHamburgerIcon}></div>
+                </div>
+            </div>
         </div>
     );
 };
