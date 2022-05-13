de1ef3360262306d393c32c818e0396d0f980143
Krystian Stopka <kr6stopka@gmail.com>
2022-05-13 14:13:03 +0200

    change structure

--- src/components/Views/HomePage/index.tsx
+++ src/components/Views/HomePage/index.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from "react";
 import { HomePageProps } from "./types.d";
 
 import styles from "./rwd.module.scss";
-import { About, Contact, Hobby, Home, Projects } from "../../Home";
+import { About, Contact, Hobby, Home, Projects } from "../../HomePage";
 
 const { homePage } = styles;
 
